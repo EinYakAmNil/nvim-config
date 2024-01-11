d4d00e75ec0e071bf69802cd4e97196a50611786
EinYakAmNil <einyak@gmail.com>
2024-01-12 00:59:52 +0100

    add python lsp setup

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -79,3 +79,60 @@ lspconfig.gopls.setup({
 		on_attach(go_settings)
 	end
 })
+
+
+function _G.python_foldexpr(lnum)
+	local fold_ranges = {}
+	local node = vim.treesitter.get_node({ pos = { 0, 1 } })
+	local i = 1
+	while node do
+		local start_row, _, end_row = node:range()
+		local type = node:type()
+
+		if type == "class_definition" then
+			for child in node:iter_children() do
+				if child:type() == "block" then
+					for cc in child:iter_children() do
+						if cc:type() == "function_definition" then
+							start_row, _, end_row = cc:range()
+							fold_ranges[i] = { start_row + 1, end_row + 2 }
+							i = i + 1
+						end
+					end
+				end
+			end
+		else
+			fold_ranges[i] = { start_row + 1, end_row + 2 }
+			i = i + 1
+		end
+
+		node = node:next_sibling()
+	end
+
+	for _, range in ipairs(fold_ranges) do
+		if lnum > range[1] and lnum < range[2] then
+			return 1
+		end
+	end
+	return 0
+end
+
+local python_buf_opts = {
+	foldmethod = "expr",
+	foldexpr = "v:lua._G.python_foldexpr(v:lnum)",
+	foldlevel = 0,
+	foldenable = true,
+}
+local python_keymaps = copy_values(general_keymaps)
+python_keymaps[#python_keymaps + 1] = { "n", "<F5>", ":w<CR>:!python %<CR>", keymap_opt }
+local python_settings = {
+	keymaps = python_keymaps,
+	buf_opts = python_buf_opts,
+}
+
+require("lspconfig").pylsp.setup({
+	capabilities = capabilities,
+	on_attach = function()
+		on_attach(python_settings)
+	end,
+})
