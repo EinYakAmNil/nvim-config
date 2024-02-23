994fe9846f51656995618f3379109936042251df
EinYakAmNil <einyak@gmail.com>
2024-02-23 22:39:38 +0100

    add image rendering plugin

---
+++ lua/plugins/image.lua
@@ -0,0 +1,44 @@
+-- required for luarocks magick
+package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua;"
+package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua;"
+
+return {
+	"3rd/image.nvim",
+	keys = {
+		{
+			"<C-L>",
+			function()
+				local image = require("image")
+				image.clear()
+			end,
+			mode = "n",
+			silent = true
+		},
+	},
+	config = function()
+		require("image").setup({
+			backend = "kitty",
+			integrations = {
+				markdown = {
+					enabled = true,
+					clear_in_insert_mode = false,
+					download_remote_images = false,
+					only_render_image_at_cursor = false,
+					filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+				},
+				neorg = {
+					enabled = true,
+					clear_in_insert_mode = false,
+					download_remote_images = true,
+					only_render_image_at_cursor = false,
+					filetypes = { "norg" },
+				},
+			},
+			window_overlap_clear_enabled = false,                            -- toggles images when windows are overlapped
+			window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+			editor_only_render_when_focused = false,                         -- auto show/hide images when the editor gains/looses focus
+			tmux_show_only_in_active_window = false,                         -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+			hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" }, -- render image files as images when opened
+		})
+	end
+}
