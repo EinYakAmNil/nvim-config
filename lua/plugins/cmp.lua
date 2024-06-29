d844facc7447e30e3cfefc5acafd04c3fa67beba
EinYakAmNil <einyak@gmail.com>
2024-06-29 21:46:36 +0200

    reorder parameters

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,11 +6,6 @@ return {
 			local lspkind = require 'lspkind'
 			local cmp = require 'cmp'
 			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require('luasnip').lsp_expand(args.body)
-					end,
-				},
 				mapping = cmp.mapping.preset.insert({
 					['<C-u>'] = cmp.mapping.scroll_docs(-4),
 					['<C-d>'] = cmp.mapping.scroll_docs(4),
@@ -41,7 +36,12 @@ return {
 						ellipsis_char = '...',
 						show_labelDetails = true,
 					})
-				}
+				},
+				snippet = {
+					expand = function(args)
+						require('luasnip').lsp_expand(args.body)
+					end,
+				},
 			})
 
 			vim.lsp.omnifunc = cmp.complete()
