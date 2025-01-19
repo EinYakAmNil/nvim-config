b3bf3f5cbed3f5518bb224de97a38096196676df
EinYakAmNil <linkai.zhang@einyakamnil.xyz>
2025-01-19 14:36:57 +0100

    open pdf for latex files

--- lua/plugins/lspconfig/latex.lua
+++ lua/plugins/lspconfig/latex.lua
@@ -3,6 +3,7 @@ local utils = require("plugins.lspconfig.utils")
 local latex_buf_opts = {}
 local latex_keymaps = utils.copy_values(utils.general_keymaps)
 latex_keymaps[#latex_keymaps + 1] = { "n", "<F5>", "<cmd>w<cr><cmd>!pdflatex %<cr>", utils.keymap_opt }
+latex_keymaps[#latex_keymaps + 1] = { "n", "<F6>", "<cmd>!zathura %:p:r.pdf disown<cr>", utils.keymap_opt }
 
 return {
 	keymaps = latex_keymaps,
