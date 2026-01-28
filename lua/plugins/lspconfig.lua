4830e9aa36c963bee06c06b8c50aa3f82dd2a639
EinYakAmNil <linkai.zhang@einyakamnil.xyz>
2026-01-29 00:17:00 +0100

    update lsp config

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,78 @@
+local lsp_keymap_opts = { noremap = true, silent = true, buffer = 0 }
+local lsp_keymaps = {
+	{ "n", "<leader>=", vim.lsp.buf.format,          lsp_keymap_opts },
+	{ "n", "<leader>e", function()
+		vim.diagnostic.jump({ count = 1, float = true })
+	end, lsp_keymap_opts },
+	{ "n", "<leader>E", function()
+		vim.diagnostic.jump({ count = -1, float = true })
+	end, lsp_keymap_opts },
+	{ "n", "<leader>K", vim.lsp.buf.hover,           lsp_keymap_opts },
+	{ "n", "<leader>r", vim.lsp.buf.rename,          lsp_keymap_opts },
+	{ "n", "gd",        vim.lsp.buf.definition,      lsp_keymap_opts },
+	{ "n", "gi",        vim.lsp.buf.implementation,  lsp_keymap_opts },
+	{ "n", "gr",        vim.lsp.buf.references,      lsp_keymap_opts },
+	{ "n", "gt",        vim.lsp.buf.type_definition, lsp_keymap_opts },
+}
+local lsp_configs = {
+	ansiblels = {},
+	bashls = {},
+	ccls = {},
+	gopls = {},
+	hls = { filetypes = { 'haskell', 'lhaskell', 'cabal' }, },
+	lua_ls = {},
+	pylsp = {},
+	terraformls = { filetypes = { "terraform", "hcl" } },
+	texlab = {},
+	ts_ls = {},
+	yamlls = {},
+}
+
+---@return string
+function _G.my_foldtext()
+	local first_line = vim.fn.getline(vim.v.foldstart)
+		:gsub("\t", string.rep(" ", vim.o.tabstop))
+	local last_line = vim.fn.getline(vim.v.foldend)
+		:gsub("\t", string.rep(" ", vim.o.tabstop))
+	local line_count = vim.v.foldend - vim.v.foldstart + 1 .. " lines"
+
+	local winwidth = vim.fn.winwidth(0)
+	local filler_len = winwidth -
+		vim.fn.strdisplaywidth(first_line) -
+		vim.fn.strdisplaywidth(last_line) - 6 -- number line column width
+
+	if filler_len <= 0 then
+		return first_line .. last_line
+	end
+
+	local fold_char = "·"
+	if #line_count + 2 < filler_len then
+		local start = math.floor((filler_len - #line_count) / 2) + 1
+		local filler = string.rep(fold_char, start - 1) ..
+			line_count ..
+			string.rep(fold_char, filler_len - #line_count - start + 1)
+		return first_line .. filler .. last_line
+	end
+
+	return first_line .. string.rep(fold_char, filler_len) .. last_line
+end
+
+return {
+	"neovim/nvim-lspconfig",
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	event = "FileType",
+	config = function()
+		vim.opt.signcolumn = "yes"
+		vim.opt.foldmethod = 'expr'
+		vim.opt.foldexpr = 'v:lua.vim.lsp.foldexpr()'
+		vim.opt.foldtext = "v:lua.my_foldtext()"
+		for _, keymap in ipairs(lsp_keymaps) do
+			---@diagnostic disable-next-line: param-type-mismatch
+			vim.keymap.set(unpack(keymap))
+		end
+		for lsp, config in pairs(lsp_configs) do
+			vim.lsp.config(lsp, config)
+			vim.lsp.enable(lsp)
+		end
+	end
+}
