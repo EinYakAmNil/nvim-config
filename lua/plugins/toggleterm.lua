7ed75991fb59660093937e3b7f9ed735cf4f41c8
EinYakAmNil <linkai.zhang@einyakamnil.xyz>
2026-05-20 21:32:11 +0200

    merge keymap

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,8 +3,7 @@ return {
 	config = function()
 		require("toggleterm").setup()
 
-		vim.keymap.set("n", "<c-t>", "<cmd>ToggleTerm direction=float<cr>", { silent = true })
-		vim.keymap.set("t", "<c-t>", "<cmd>ToggleTerm direction=float<cr>", { silent = true })
+		vim.keymap.set({ "n", "t" }, "<c-t>", "<cmd>ToggleTerm direction=float<cr>", { silent = true })
 		vim.keymap.set("t", "<c-q>", "<c-\\><c-n>", { noremap = true })
 	end
 }
